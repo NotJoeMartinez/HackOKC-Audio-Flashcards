f3d2d8d180d5e8aaf6a8dd02a1125f5ac3a1501e
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2020-02-09 00:13:35 -0600

    crud is operationalish, not the api or audio editing tho

--- app.py
+++ app.py
@@ -1,58 +1,83 @@
-import os
-from flask import Flask, render_template, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
-from flask import send_from_directory
-from forms import KeyWordForm
-from flask_sqlalchemy import SQLAlchemy
-
-
-
+from flask import Flask 
+from flask import render_template, request, flash, redirect, url_for
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SECRET_KEY'] = "yeet4321"
-db = SQLAlchemy(app)
 
+app.config['SECRET_KEY'] = 'yeet4321'
 
+@app.route('/')
+def home(): 
+    # if request.method == 'POST':  
+    #     f = request.files['file']  
+    #     f.save('uploads/' + f.filename)  
 
-AUDIO_UPLOAD_FOLDER = 'uploads/audio'
-TEXT_UPLOAD_FOLDER = 'uploads/text'
-ALLOWED_EXTENSIONS = {'txt','mp3'}
+    #     # upload the the audio to google cloud and return the uri 
+    #     upload_blob(cf.bucketname, 'uploads/'+f.filename , f.filename)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    #     # Build uri suing the filename suplied by user 
+    #     gcs_uri = 'gs://' + cf.bucketname + '/' + f.filename
 
-# Get files from user 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file.save(os.path.join(AUDIO_UPLOAD_FOLDER, filename))
-            return redirect(url_for('uploaded_audio_file', filename=filename))
+    #     # execute transcribe function on the gcs_uri save the transcript to full_transcript
+    #     full_transcript = sample_long_running_recognize(gcs_uri, f.filename)
+    
+    # # Return template success.html save the name & contents of file to vars 
+    # return render_template("success.html", name = f.filename, text = full_transcript)  
+    
     return render_template('home.html')
 
-@app.route('/uploads/audio/<filename>')
-def uploaded_audio_file(filename):
-    return send_from_directory(AUDIO_UPLOAD_FOLDER, filename)
+
+# start of form classes
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, HiddenField, StringField, IntegerField
+
+class PostformForm(FlaskForm):
+    id = HiddenField()
+    postTitle = StringField('Lecture Title')
+    postBody = StringField('Comma Sepparated Keywords')
+    submit = SubmitField("Save")
+
+# Start of database classes
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/employee.db"
+db = SQLAlchemy(app)
+
+class Postform(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    postTitle = db.Column(db.String)
+    postBody = db.Column(db.String)
+    references = db.Column(db.String)
+    
+    def __repr__(self):
+        return "(%r, %r)" %(self.postTitle,self.postBody)
+
+# view
 
 
-@app.route('/keywords', methods=['GET', 'POST'])
-def keywords():
-    form = KeyWordForm()
+@app.route("/postform", methods=["GET", "POST"])
+def createPostform():
+    form = PostformForm(request.form)
+    postforms = Postform.query.all()
     if form.validate_on_submit():
-        return redirect(url_for('success'))
-    return render_template('keywords.html', form=form)
+        postform = Postform(postTitle=form.postTitle.data, postBody=form.postBody.data)
+        db.session.add(postform)
+        db.session.commit()
+        flash("Added Postform Successfully")
+        return redirect(url_for("createPostform"))
+    return render_template("postform.html", title="Postform", form=form, postforms=postforms)
 
-   
+@app.route("/updatePostform/<int:postform_id>", methods=["GET", "POST"])
+def updatePostform(postform_id):
+    postform = Postform.query.get(postform_id)
+    form = PostformForm(request.form, obj=postform)
+    if form.validate_on_submit():
+        form.populate_obj(postform)
+        db.session.commit()
+        flash("Updated Postform Successfully")
+        return redirect(url_for("createPostform"))
+    return render_template("postform.html", title="Postform", form=form, postforms=Postform.query.all())
+
+@app.route("/deletePostform/<int:postform_id>", methods=["GET", "POST"])
+def deletePostform(postform_id):
+    postform = Postform.query.get(postform_id)
+    db.session.delete(postform)
+    db.session.commit()
+    return redirect(url_for("createPostform"))
